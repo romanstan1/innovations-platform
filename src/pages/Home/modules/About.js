85ded426098b1bba6881394790060fc3c7f8f6c8
Roman Stankiewicz <roman.stan3@gmail.com>
2018-08-01 13:35:30 +0100

    updated copy

--- src/pages/Home/modules/About.js
+++ src/pages/Home/modules/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 export default () =>
 <div className='About'>
-  <span>Lorem ipsum</span> dolor sit amet, consectetur adipisicing elit. Expedita, <span>voluptatem</span> veniam! Molestiae ipsum voluptatum unde illum animi cupiditate beatae doloremque totam architecto odit <span>repellendus nemo</span> repellendus, nemo, dignissimos reprehenderit culpa nihil sint.
-  <br/>
-  <br/>
-  Ipsum voluptatum unde illum animi cup
+  “An obstacle downstream propagates upstream. If you’re not allowed to implement new ideas, you stop having them. And vice versa: when you can do whatever you want, you have more ideas about what to do. So working for yourself makes your brain more powerful in the same way a low-restriction exhaust system makes an engine more powerful.” - Paul Graham
+  <br/><br/>
+  In a fast-moving digital world, the ability to figure out what is going on and understand the layers of complexity is essential. Using that knowledge to establish a better order is powerful.
+  <br/><br/>
+  We spend time thinking about how things work and how they might be made better. Sometimes we turn those ideas into projects, be it for a client or just for us.
+  <br/><br/>
+  Here is a selection of some of those side projects and thinking.
 </div>
