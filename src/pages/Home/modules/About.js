d25e3a6777247a4ebb70eb8aa1614afa2a45ecea
Roman Stankiewicz <roman.stan3@gmail.com>
2018-08-01 13:56:26 +0100

    updated copy

--- src/pages/Home/modules/About.js
+++ src/pages/Home/modules/About.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 export default () =>
 <div className='About'>
-  “An obstacle downstream propagates upstream. If you’re not allowed to implement new ideas, you stop having them. And vice versa: when you can do whatever you want, you have more ideas about what to do. So working for yourself makes your brain more powerful in the same way a low-restriction exhaust system makes an engine more powerful.” - Paul Graham
+  In a fast-moving digital world, the ability to figure out what is going on and understand the layers of complexity is <span>essential</span>. Using that knowledge to establish a better order is <span>powerful</span>.
   <br/><br/>
-  In a fast-moving digital world, the ability to figure out what is going on and understand the layers of complexity is essential. Using that knowledge to establish a better order is powerful.
-  <br/><br/>
-  We spend time thinking about how things work and how they might be made better. Sometimes we turn those ideas into projects, be it for a client or just for us.
+  We spend time thinking about how things work and how they might be made better. Sometimes we turn those <span>ideas</span> into projects, be it for a client or just for us.
   <br/><br/>
   Here is a selection of some of those side projects and thinking.
 </div>
